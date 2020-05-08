7e978a61034e27c85cc99aec2c2e1c220bbee0e8
defrindr <defrindr@gmail.com>
2020-05-08 18:28:56 +0700

    changed app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    // Sistem Informasi Absensi & Point
+    'name' => env('APP_NAME', 'SIAP APP'),
 
     /*
     |--------------------------------------------------------------------------
